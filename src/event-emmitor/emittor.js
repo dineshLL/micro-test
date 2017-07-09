afc77ff0a092ff97ba69f99c0eb275d9a46b8038
dineshLL <dineshgitt@gmail.com>
2017-07-09 14:45:36 +0530

    feat(data emmit to server) data emmiting to server

--- src/event-emmitor/emittor.js
+++ src/event-emmitor/emittor.js
@@ -1,27 +1,77 @@
 var exec = require('child_process').exec;
+var http = require('http')
 var containerId
 
-module.exports = function(imas) {
+module.exports = function (imas) {
     return new Promise((resolve, reject) => {
-        console.log('this is the hash: ' + imas.hash)
-        console.log('this is getting emmited by the function' + getDockerContainerId())
+        getDockerContainerId()
+            .then(id => {
+                saveUrlMeta(imas)
+            })
+            .catch(err => reject(err))
+
         resolve()
     })
-    
+
 }
 
 function getDockerContainerId() {
-    if(!containerId) {
-       exec('cat \/proc\/self\/cgroup | grep \"cpu:\/\" | sed \'s\/\\([0-9]\\):cpu:\\\/docker\\\/\/\/g\'', setContainerId)
-    }
-    
-    return containerId
+    return new Promise((resolve, reject) => {
+        if (containerId) {
+            console.log('container id is already resolved')
+            resolve(containerId)
+        } else {
+            console.log('resolving the container id')
+            exec('cat \/proc\/self\/cgroup | grep \"cpu:\/\" | sed \'s\/\\([0-9]\\):cpu:\\\/docker\\\/\/\/g\'',
+                function (error, stdout, stderr) {
+                    if (!error) {
+                        containerId = stdout
+                        resolve(containerId);
+                    } else {
+                        reject(error)
+                    }
+                })
+        }
+    })
+
+
+    //cat \/proc\/self\/cgroup | grep \"cpu:\/\" | sed \'s\/\\([0-9]\\):cpu:\\\/docker\\\/\/\/g\'
 }
 
-function setContainerId(error, stdout, stderr) { 
-    if(!error) {
-        containerId = stdout
-    }
-    console.log(stdout)
+
+function saveUrlMeta(imas) {
+    imas.inboundType = "2"
+    console.log('container id is : ', containerId)
+    imas.containerId = containerId
+
+    console.log('this is saveUrlMeta method' + JSON.stringify(imas))
+
+    var data = JSON.stringify(imas)
+    return new Promise((resolve, reject) => {
+        var options = {
+            host: 'localhost',
+            port: 8080,
+            path: '/imas-data-collector/api/data',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(data)
+            }
+        };
+
+        const req = http.request(options, function (res) {
+            console.log('STATUS: ' + res.statusCode);
+            //console.log('HEADERS: ' + JSON.stringify(res.headers))
+            res.setEncoding('utf8')
+            res.on('data', function (chunk) {
+                //console.log('BODY: ' + chunk);
+            })
+        })
+
+        req.write(data)
+        req.end()
+
+        resolve()
+    });
 }
 
